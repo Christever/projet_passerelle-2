bbf1d4cba8731e5c5beaec11de16ad94b0873f4a
Christever <christian@steverlynck.fr>
2023-03-06 18:06:49 +0100

    suppression user-scalable=no

--- includes/header.php
+++ includes/header.php
@@ -3,16 +3,15 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link rel="stylesheet" href="public/css/style.css">
     <title><?=$title?></title>
 </head>
 
-<body class="d-flex vh-100 flex-column" >
+<body class="d-flex vh-100 flex-column">
 
-<header class="bg-secondary text-white text-center">
-    <?php include "includes/navbar.php" ?>
-</header>
+    <header class="bg-secondary text-white text-center">
+        <?php include "includes/navbar.php" ?>
+    </header>
