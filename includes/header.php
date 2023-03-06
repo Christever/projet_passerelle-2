ef4758471c136796176af536b432d4ca0fb18da9
Christever <christian@steverlynck.fr>
2023-03-06 18:18:28 +0100

    ajout meta description

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=6.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <meta name="description" content="Author: Christever">
     <link rel="stylesheet" href="public/css/style.css">
     <title><?=$title?></title>
 </head>
